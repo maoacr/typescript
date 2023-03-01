5747f7adec488e0a591e5bb5b2f08d8e31733d56
Mao <maoacr@gmail.com>
2023-03-01 13:52:53 -0500

    bases folder

---
+++ bases/app.js
@@ -0,0 +1,6 @@
+var mensaje = 'Mensaje'; //Esto dato dice ser de tipo 'Hola mundo'
+var msj = 'Hola mundo'; // Al pasarlo a let, esto dato dice ser de tipo: string
+msj = 'Mao'; //No se queja porque recibe string
+msj = 123; //Marca error por el tipo de dato
+var name = 'Mao';
+console.log(name);
---
+++ bases/app.ts
@@ -0,0 +1,8 @@
+const mensaje = 'Mensaje' //Esto dato dice ser de tipo 'Mensaje' porque esta infiriendo el dato
+let msj = 'Hola mundo'; // Al pasarlo a let, esto dato dice ser de tipo: string
+msj = 'Mao' //No se queja porque recibe string
+msj = 123 //Marca error por el tipo de dato
+
+
+const name: string = 'Mao';
+console.log(name);
